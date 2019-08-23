480f4eba8e52f04e134ed60638aa3fdee74217b2
abhik-b <abhik0301@gmail.com>
2019-08-23 17:54:16 +0530

    5th commit

---
+++ automating_calcu/automating_calcu.py
@@ -0,0 +1,7 @@
+import pyautogui,time
+time.sleep(5)
+
+pyautogui.click(pyautogui.locateCenterOnScreen('1key.png'))
+pyautogui.click(pyautogui.locateCenterOnScreen('plus.png'))
+pyautogui.click(pyautogui.locateCenterOnScreen('1key.png'))
+pyautogui.click(pyautogui.locateCenterOnScreen('equalkey.png'))
